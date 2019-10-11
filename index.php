ca7589a442f447e45dc5af301b3ec8a695875d9b
Ranjeet <ranjeet@gmail.com>
2019-10-11 16:57:34 +0530

    First commit

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * A pseudo-CRON daemon for scheduling WordPress tasks
+ *
+ * WP Cron is triggered when the site receives a visit. In the scenario
+ * where a site may not receive enough visits to execute scheduled tasks
+ * in a timely manner, this file can be called directly or via a server
+ * CRON daemon for X number of times.
+ *
+ * Defining DISABLE_WP_CRON as true and calling this file directly are
+ * mutually exclusive and the latter does not rely on the former to work.
+ *
+ * The HTTP request to this file will not slow down the visitor who happens to
+ * visit when the cron job is needed to run.
+ *
+ * @package WordPress
+ */
